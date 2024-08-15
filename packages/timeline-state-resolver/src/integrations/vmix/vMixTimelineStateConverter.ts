8be5b331e92c11f910ed457aa747ff990fc7e9d6
ianshade <ianshade@users.noreply.github.com>
2024-08-15 19:30:04 +0200

    feat(EAV-54): allow overriding vMix transition from a separate layer
    one object (on an alphabetically higher layer) can be used to select the transition, and another to select the input

--- packages/timeline-state-resolver/src/integrations/vmix/vMixTimelineStateConverter.ts
+++ packages/timeline-state-resolver/src/integrations/vmix/vMixTimelineStateConverter.ts
@@ -71,6 +71,11 @@ export class VMixTimelineStateConverter {
 								this._switchToInput(content.input, deviceState, mixProgram, content.transition)
 							} else if (content.inputLayer) {
 								this._switchToInput(content.inputLayer, deviceState, mixProgram, content.transition, true)
+							} else if (content.transition) {
+								const mixState = deviceState.reportedState.mixes[mixProgram]
+								if (mixState) {
+									mixState.transition = content.transition
+								}
 							}
 						}
 						break
@@ -236,7 +241,7 @@ export class VMixTimelineStateConverter {
 			mixState.preview = mixState.program
 			mixState.program = input
 
-			mixState.transition = transition || { effect: VMixTransitionType.Cut, duration: 0 }
+			mixState.transition = transition ?? { effect: VMixTransitionType.Cut, duration: 0 }
 			mixState.layerToProgram = layerToProgram
 		}
 	}
